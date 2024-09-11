7ded89cc4992b59feeb889841868d57060e441ab
SK-0103 <115455602+SK-0103@users.noreply.github.com>
2024-09-11 18:33:18 +0530

    2D matrix

---
+++ matrix.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        int[][] arr = new int[3][2];
+
+        for (int row = 0; row<arr.length; row++){
+            for (int col=0; col < arr[row].length; col++){
+                arr[row][col] = in.nextInt();
+            }  
+        }
+        // for (int row = 0; row<arr.length; row++){
+        //     for (int col=0; col < arr[row].length; col++){
+        //         System.out.println(arr[row][col] + " ");
+        //     }  
+        //     System.out.println();
+        // }
+
+                        //or
+
+        for (int row = 0; row<arr.length; row++){
+            System.out.println(Arrays.toString(arr[row]));
+        }
+        in.close();
+    }
+}
